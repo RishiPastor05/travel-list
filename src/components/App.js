9b2bb0d18a2ab4c95196407718871533d873d04c
rishi pastor <rishipastor1607@gmail.com>
2024-09-27 16:58:12 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "charger", quantity: 1, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    if (window.confirm("Are you sure you want to delete all items?"))
+      setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItems}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/components/Stats.js
@@ -0,0 +1,21 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🛩️</em>
+      </footer>
+    );
+  const itemsLength = items.length;
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((itemsPacked / itemsLength) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentPacked === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 You have ${itemsLength} items on your list, and you already packed
+        ${itemsPacked}(${percentPacked}%)`}
+      </em>
+    </footer>
+  );
+}
